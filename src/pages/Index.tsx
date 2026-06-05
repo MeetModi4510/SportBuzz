7b9d356f9d203dad5e204c36ec81b4ba78ecb6bf
Meet Modi <mmeet0091@gmail.com>
2026-06-05 15:19:19 +0530

    Light Mode Implementation

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -192,7 +192,7 @@ const Index = () => {
         />
       </Helmet>
 
-      <div className="min-h-screen bg-background">
+      <div className="min-h-screen">
         <Navbar />
 
         <main className="container mx-auto px-4 py-6 space-y-8">
